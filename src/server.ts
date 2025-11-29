c795be48d79e677cf3873109605cc6bee042bdc4
PIASH <zmnobab1999@gmail.com>
2025-11-29 23:08:15 +0600

    added post method

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,20 @@ import express, { Request, Response } from "express";
 const app = express();
 const port = 5000;
 
+// parser
+app.use(express.json());
+// app.use(express.urlencoded());
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello Next Level Web Developer!");
+});
+
+app.post("/", (req: Request, res: Response) => {
+  console.log(req.body);
+  res.status(201).json({
+    success: true,
+    message: "API is working",
+  });
 });
 
 app.listen(port, () => {
