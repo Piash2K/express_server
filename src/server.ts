839d7bbcb10c12a546c2631483f3a515d40a9d5b
PIASH <zmnobab1999@gmail.com>
2025-11-29 23:28:34 +0600

    connect with database using pool

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,30 @@ import express, { Request, Response } from "express";
 import { Pool } from "pg";
 const app = express();
 const port = 5000;
+// parser
+app.use(express.json());
+// app.use(express.urlencoded());
 
 const pool = new Pool({
   connectionString: `postgresql://neondb_owner:npg_ve4mtq0ykDUf@ep-quiet-tooth-a4qp59h7-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`,
 });
 
-// parser
-app.use(express.json());
-// app.use(express.urlencoded());
+const initDB = async () => {
+  await pool.query(`
+        CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR (100) NOT NULL,
+        email VARCHAR (150) UNIQUE NOT NULL,
+        age INT,
+        phone VARCHAR(15),
+        address TEXT,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+        )
+        `);
+};
+
+initDB()
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello Next Level Web Developer!");
