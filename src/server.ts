25a92ce44e0a62fed7e707eb3096e8488c337f90
PIASH <zmnobab1999@gmail.com>
2025-11-30 10:50:03 +0600

    post and get method added for todos

--- src/server.ts
+++ src/server.ts
@@ -116,9 +116,12 @@ app.get("/users/:id", async (req: Request, res: Response) => {
 });
 app.put("/users/:id", async (req: Request, res: Response) => {
   //   console.log(req.params.id)
-  const {name,email}= req.body;
+  const { name, email } = req.body;
   try {
-    const result = await pool.query(`UPDATE users SET name=$1, email=$2 WHERE id=$3 RETURNING *`,[name,email,req.params.id]);
+    const result = await pool.query(
+      `UPDATE users SET name=$1, email=$2 WHERE id=$3 RETURNING *`,
+      [name, email, req.params.id]
+    );
     if (result.rows.length === 0) {
       res.status(404).json({
         success: false,
@@ -157,6 +160,43 @@ app.delete("/users/:id", async (req: Request, res: Response) => {
         data: result.rows,
       });
     }
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      details: error,
+    });
+  }
+});
+
+//todo crud
+app.post("/todos", async (req: Request, res: Response) => {
+  const { user_id, title } = req.body;
+  try {
+    const result = await pool.query(
+      `INSERT INTO todos (user_id,title) VALUES($1,$2) RETURNING *`,
+      [user_id, title]
+    );
+    res.status(201).json({
+      success: true,
+      message: "Todo created",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+app.get("/todos", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * FROM todos`);
+    res.status(200).json({
+      success: true,
+      message: "todos retrieved successfully",
+      data: result.rows,
+    });
   } catch (error: any) {
     res.status(500).json({
       success: false,
