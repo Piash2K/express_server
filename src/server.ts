04b87e931e230487ee3029b60f437027b6ee0a09
PIASH <zmnobab1999@gmail.com>
2025-12-04 23:59:56 +0600

    creating password field and hashing password

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import initDB, { pool } from "./config/db";
 import logger from "./middleware/logger";
 import { userRoutes } from "./modules/user/user.routes";
 import { todoRoutes } from "./modules/todo/todo.routes";
+import { authRoutes } from "./modules/auth/auth.routes";
 
 const app = express();
 const port = config.port;
@@ -23,8 +24,10 @@ app.get("/", logger, (req: Request, res: Response) => {
 app.use("/users", userRoutes);
 
 //todo crud
-app.use("/todos", todoRoutes)
+app.use("/todos", todoRoutes);
 
+//auth routes
+app.use("/auth", authRoutes);
 
 app.use((req, res) => {
   res.status(404).json({
